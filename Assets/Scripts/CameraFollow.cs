e3b42d205b8183a7d3104a1b26be63ff61be082c
AnkurSheel <ankur.sheel@gmail.com>
2016-01-24 12:09:53 +1300

    - basic level setup

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+  private GameObject player;
+  public float CameraSpeed = 5.0f;
+
+	void Start () {
+    player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void FixedUpdate () {
+    Vector3 campos = transform.position;
+    campos.z = player.transform.position.z - 3.0f;
+    transform.position = Vector3.Lerp(transform.position, campos, 3 * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+  public static int PlayerSpeed = 10;
+
+	void FixedUpdate () 
+  {
+    gameObject.transform.Translate(gameObject.transform.forward * PlayerSpeed * Time.fixedDeltaTime);
+	}
+}
