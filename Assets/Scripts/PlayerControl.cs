d41740c2eadc385d526f034c0682404d2a35afe6
AnkurSheel <ankur.sheel@gmail.com>
2016-01-24 13:06:23 +1300

    - adding player input to move left  and right

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+  public int PlayerSpeed = 10;
+  private Vector3 dir = Vector3.zero;
+
+  void Start()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    float amountToMove = PlayerSpeed * Time.fixedDeltaTime;
+    transform.Translate(dir * amountToMove);
+  }
+
+  void Update()
+  {
+    dir = transform.forward;
+
+    if (Input.GetAxis("Horizontal") < 0)
+    {
+      dir -= transform.right;
+    }
+    else if (Input.GetAxis("Horizontal") > 0)
+    {
+      dir += transform.right;
+    }
+  }
+}
