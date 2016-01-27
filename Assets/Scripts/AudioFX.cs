4713990319aea31a7c88f94a724a2a15e573170d
AnkurSheel <ankur.sheel@gmail.com>
2016-01-27 19:38:56 +1300

    - added background music and coin collect sfx

---
+++ Assets/Scripts/AudioFX.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFX : MonoBehaviour
+{
+  public AudioClip collect;
+
+  public void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.tag == "Coin")
+    {
+      GetComponent<AudioSource>().PlayOneShot(collect, 1.0f);
+    }
+  }
+}
