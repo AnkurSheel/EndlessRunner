8bd2719591513adf211599576b0220633aa806b8
AnkurSheel <ankur.sheel@gmail.com>
2016-01-27 18:41:10 +1300

    - auto destroying coins and enemies

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour
+{
+  public float Delay = 30.0f;
+
+  void Start()
+  {
+    StartCoroutine(DestroyMe());
+  }
+
+  IEnumerator DestroyMe()
+  {
+    yield return new WaitForSeconds(Delay);
+    Destroy(this.gameObject);
+  }
+}
